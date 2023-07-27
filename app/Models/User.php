660aac89a0271a74d9d1d8916ef5999a83c8448e
Faris Fadhail <farisfadhail@gmail.com>
2023-07-27 12:44:06 +0700

    fix: preening code

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,8 @@ class User extends Authenticatable
         'name',
         'age',
         'city',
+        //'email',
+        //'password'
     ];
 
     /**
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+
             $table->string('name');
             $table->string('age');
             $table->string('city');
@@ -20,6 +21,7 @@ return new class extends Migration
             //$table->timestamp('email_verified_at')->nullable();
             //$table->string('password');
             //$table->rememberToken();
+
             $table->timestamps();
         });
     }
